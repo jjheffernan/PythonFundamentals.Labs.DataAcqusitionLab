464157a11b08f0fbad29dbb062dba9df28b40a44
jjheffernan <jjheffernan99@gmail.com>
2022-08-01 16:14:13 -0400

    started code outline

---
+++ make_requests.py
@@ -0,0 +1,8 @@
+from urllib import request
+
+# using urllib make rest calls to NOAA's API and save the results to json files
+
+# once executed, there should be 39 files.
+# JSON objects named 'locations_x.json' numbered 0-38
+
+# Be careful to overexchaust the alloted calls due to bugs
